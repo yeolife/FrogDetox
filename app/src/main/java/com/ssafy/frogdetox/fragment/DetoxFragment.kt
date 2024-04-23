41c431cadbcf99f04d61b811a52e21bb28221978
yeochangmin1 <yeo2507@gmail.com>
2024-04-24 00:16:34 +0900

    DetoxFragment를 메인 액티비티에 붙임

--- app/src/main/java/com/ssafy/frogdetox/fragment/DetoxFragment.kt
+++ app/src/main/java/com/ssafy/frogdetox/fragment/DetoxFragment.kt
@@ -1,20 +1,35 @@
 package com.ssafy.frogdetox.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.ssafy.frogdetox.R
+import com.ssafy.frogdetox.MainActivity
+import com.ssafy.frogdetox.databinding.FragmentDetoxBinding
+
 class DetoxFragment : Fragment() {
+    private lateinit var mainActivity: MainActivity
+    private var _binding: FragmentDetoxBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        mainActivity = context as MainActivity
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_detox, container, false)
+    ): View {
+        _binding = FragmentDetoxBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 }
