2bd10ac7927a16301cd4d3ae5f28fc554bd722d2
yeochangmin1 <yeo2507@gmail.com>
2024-05-08 02:18:23 +0900

    FireBase 연동

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     id ("org.jetbrains.kotlin.kapt")
-    id("com.google.gms.google-services")
+    id("com.google.gms.google-services");
 }
 
 android {
